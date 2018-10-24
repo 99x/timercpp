58a3859455ccc5af798977eaf0bdad56548572b8
Shalitha <shalithasuranga@gmail.com>
2018-10-24 13:02:58 +0530

    Adding project files :rocket:

---
+++ timercpp.h
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+class Timer {
+    bool clear = false;
+
+    public:
+        void setTimeout(auto function, int delay);
+        void setInterval(auto function, int interval);
+        void stop();
+
+};
+
+void Timer::setTimeout(auto function, int delay) {
+    this->clear = false;
+    std::thread t([=]() {
+        if(this->clear) return;
+        std::this_thread::sleep_for(std::chrono::milliseconds(delay));
+        function();
+    });
+    t.detach();
+}
+
+void Timer::setInterval(auto function, int interval) {
+    this->clear = false;
+    std::thread t([=]() {
+        while(true) {
+            if(this->clear) return;
+            std::this_thread::sleep_for(std::chrono::milliseconds(interval));
+            function();
+        }
+    });
+    t.detach();
+}
+
+void Timer::stop() {
+    this->clear = true;
+}
